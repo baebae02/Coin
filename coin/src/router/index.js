da466a2fc93677f21ef8aaf420d5603e357c8c70
Nayeon Kim <inourbubble2@gmail.com>
2021-11-16 17:13:33 +0900

    feat: change file name

--- coin/src/router/index.js
+++ coin/src/router/index.js
@@ -1,29 +1,29 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Intro from '../views/Intro.vue';
-import Search from '../views/search.vue';
-import Add from '../views/add.vue';
-import Star from '../views/star.vue';
+import Search from '../views/Search.vue';
+import Add from '../views/Add.vue';
+import Star from '../views/Star.vue';
 const routes = [
   {
     path: '/',
     name: 'Intro',
     component: Intro,
-  }, 
+  },
   {
     path: '/search',
     name: 'Search',
     component: Search,
-  }, 
+  },
   {
     path: '/add',
     name: 'Add',
     component: Add,
-  }, 
+  },
   {
     path: '/star',
     name: 'Star',
     component: Star,
-  }, 
+  },
 ];
 
 const router = createRouter({
