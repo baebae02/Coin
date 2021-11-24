0e56796947c7afc3027a69cfd62b98f2f9030f08
Nayeon Kim <inourbubble2@gmail.com>
2021-11-25 02:37:41 +0900

    feat: logout 구현

--- coin/src/components/Login.vue
+++ coin/src/components/Login.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="container">
-    <label>카카오로 로그인할 수 있어요!</label>
+
+    <label v-if="!logined">카카오로 로그인할 수 있어요!</label>
     <img class="button"
          src="../assets/kakao_login_large_narrow.png"
-         @click="loginWithKakao">
+         @click="loginWithKakao"
+         v-if="!logined">
+    <label v-else>
+      로그아웃되었어요!
+    </label>
   </div>
 
 </template>
@@ -13,9 +18,23 @@ import {KEYS} from "../utils/credentials";
 
 export default {
   name: "Login",
+  data() {
+    return {
+      logined: false
+    }
+  },
   mounted() {
     /* global Kakao */
-    Kakao.init(KEYS.JAVASCRIPT_KEY);
+    if (!Kakao.isInitialized())
+      Kakao.init(KEYS.JAVASCRIPT_KEY);
+    if (Kakao.Auth.getAccessToken()) {
+      this.logined = true
+      Kakao.Auth.logout(function() {
+        console.log(Kakao.Auth.getAccessToken());
+      });
+    } else {
+      this.logined = false
+    }
   },
   methods: {
     loginWithKakao() {
